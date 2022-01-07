9d69666858c254a2232ff8a631197bec8cdc5e4f
MC-GH <cornelismichael@hotmail.com>
2022-01-07 15:51:39 +0100

    Rock Paper scissors update

--- Rock-paper-scissors.js
+++ Rock-paper-scissors.js
@@ -1,59 +1,62 @@
 
-//Create a function which will randomly generate rock paper scissors and store it in a variable computerChoice//
-
-const possibleOptions = ["Rock", "Paper", "Scissors"];
+const possibleOptions = ["rock", "paper", "scissors"];
 
 function computerPlay() {
   return possibleOptions[Math.floor(Math.random()*possibleOptions.length)];
 }
 
-function playRound() {
-    let playerSelection = prompt("Please enter Rock, Paper or Scissors");
-    let computerSelection = computerPlay();
-
-    if (playerSelection === "Rock" && computerSelection === "Paper") {
-        console.log(`Computer chose ${computerSelection}. ${computerSelection} beats ${playerSelection}. Computer wins`);
-        // to delete console.log(computerScore + 1);//
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        console.log(`Computer chose ${computerSelection}. ${computerSelection} loses to ${playerSelection}. Player wins`);
-    } else if (playerSelection === "Rock" && computerSelection === "Rock") {
-        console.log(`Computer also chose ${computerSelection}. It's a tie!`);
-
-    }
-}
-
-//scores are not saved outside of the function//
 let playerScore = 0;
 let computerScore = 0;
 
-//Ask input from user and store it in a variable userChoice (rock paper or scissors)//
+function game() {
+    while(playerScore < 5 && computerScore < 5) {
+        if (playRound() === "computer") {
+            ++computerScore;
+            console.log(`Pc wins! Current score: Computer: ${computerScore} Player: ${playerScore}`);
+        } else if (playRound() === "player") {
+            ++playerScore;
+            console.log(`Player wins! Current score: Computer: ${computerScore} Player: ${playerScore}`);
+        } else {
+            console.log(`It's a tie! Current score: Computer: ${computerScore} Player: ${playerScore}`);
+        }
+    }
+    if (playerScore === 5) {
+        console.log("Congratulations, player wins the game!");
+    }
+    if (computerScore === 5) {
+        console.log("What a shame, computer wins the game!");
+    }
+}
 
-//Write a function that compares user input choice with random choice of computer and that keeps score in a points counter//
-    //If user chooses rock and computer chooses scissors
-        // => Display message 'user wins', add a point to the counter of user//
-    //else if user chooses rock and computer chooses paper
-        // => Display message 'computer wins', add a point to the counter of computer//
-    //else if user chooses rock and computers chooses Rock
-        // => Display message 'It's a tie! Try again', add a point to the counter of computer + user//
+function playRound() {
 
-    //else if user chooses paper and computer chooses rock
-        // => Display message 'user wins', add a point to the counter of user//
-    //else if user chooses paper and computer chooses scissors
-        // => Display message 'computer wins', add a point to the counter of computer//
-    //else if user chooses paper and computers chooses paper
-        // => Display message 'It's a tie! Try again', add a point to the counter of computer + user//
+    let playerSelectionInput = prompt("Please enter rock, paper or scissors");
+    let playerSelection = playerSelectionInput.toLowerCase();
+    let computerSelection = computerPlay();
 
-         //else if user chooses scissors and computer chooses paper
-        // => Display message 'user wins', add a point to the counter of user//
-    //else if user chooses scissors and computer chooses rock
-        // => Display message 'computer wins', add a point to the counter of computer//
-    //else if user chooses scissors and computers chooses scissors
-        // => Display message 'It's a tie! Try again', add a point to the counter of computer + user//
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "computer";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "player";
+    } else if (playerSelection === "rock" && computerSelection === "rock") {
+        return "tie";
+    
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "computer";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "player";
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        return "tie";
+    
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "computer";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "player";
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        return "tie";
+        
+    } else { 
+        console.log("I don't know this gesture.");
+    }
+}
 
-
-//create a points counter for user + a points counter for computer//
-    //if points counter of user reaches 5 and points counter of computer is less than 5, display a message, user wins//
-    //if points counter of computer reaches 5 and the points counter of user is less than 5, display a message, computer wins//
-    //if points counter of user AND computer are 5, display a message, it's a tie, would you like to try again?//
-
-//create a reset option?//
