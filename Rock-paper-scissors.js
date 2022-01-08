a437eaa892f7991c3350c4532f25bd587385facd
Michaël <cornelismichael@hotmail.com>
2022-01-08 10:04:29 +0100

    First version finished

--- Rock-paper-scissors.js
+++ Rock-paper-scissors.js
@@ -5,33 +5,48 @@ function computerPlay() {
   return possibleOptions[Math.floor(Math.random()*possibleOptions.length)];
 }
 
+function userPlay() {
+    let input = prompt("Please enter rock, paper or scissors");
+    if (input === null) {
+        return "cancel";
+    }
+    else {
+        return input.toString().toLowerCase();
+    }
+}
+
 let playerScore = 0;
 let computerScore = 0;
 
 function game() {
     while(playerScore < 5 && computerScore < 5) {
-        if (playRound() === "computer") {
+        let roundWinner = playRound();
+        if (roundWinner === "computer") {
             ++computerScore;
             console.log(`Pc wins! Current score: Computer: ${computerScore} Player: ${playerScore}`);
-        } else if (playRound() === "player") {
+        } 
+        if (roundWinner === "player") {
             ++playerScore;
             console.log(`Player wins! Current score: Computer: ${computerScore} Player: ${playerScore}`);
-        } else {
+        }
+        if (roundWinner === "tie") {
             console.log(`It's a tie! Current score: Computer: ${computerScore} Player: ${playerScore}`);
         }
-    }
-    if (playerScore === 5) {
-        console.log("Congratulations, player wins the game!");
-    }
-    if (computerScore === 5) {
-        console.log("What a shame, computer wins the game!");
-    }
+        if (roundWinner === "cancel") {
+            break ;
+        }
+    }   
+        if (playerScore === 5) {
+            console.log("Congratulations, player wins the game!");
+        }
+        if (computerScore === 5) {
+            console.log("What a shame, computer wins the game!");
+        }
 }
 
 function playRound() {
 
-    let playerSelectionInput = prompt("Please enter rock, paper or scissors");
-    let playerSelection = playerSelectionInput.toLowerCase();
+    let playerSelection = userPlay();
     let computerSelection = computerPlay();
 
     if (playerSelection === "rock" && computerSelection === "paper") {
@@ -54,9 +69,14 @@ function playRound() {
         return "player";
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
         return "tie";
-        
+
+    } else if (playerSelection === "cancel") {
+        return "cancel";
     } else { 
         console.log("I don't know this gesture.");
     }
 }
 
+
+//one issue left = if cancel is pressed it should cancel the loop and display cancelled message.//
+//works for now, but does not seem optimal? Have to mention it several times//
