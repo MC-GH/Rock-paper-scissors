958095acde489bea3e0cc3497ffd45785683875e
Michaël <cornelismichael@hotmail.com>
2022-02-01 14:03:55 +0100

    Fixed cancel issue and empty input

--- Rock-paper-scissors.js
+++ Rock-paper-scissors.js
@@ -5,43 +5,19 @@ function computerPlay() {
   return possibleOptions[Math.floor(Math.random()*possibleOptions.length)];
 }
 
+//the function computerPlay selects a random item from the array possibleOptions.
+//more specifically like this: Math.random() function returns a number between 0 and 1. 
+//this number is multiplied by the array.length. ( = 3 items ).
+//this number is rounded down to the nearest integer (whole number) (or stays the same if it's a whole number = integer)
+//this results in a result of 0, 1 or 2, which represents the index of the array items.
+
 function userPlay() {
-    let input = prompt("Please enter rock, paper or scissors");
+    let input = prompt("Please enter rock, paper or scissors. Or press cancel to cancel the game.");
     if (input === null) {
-        return "cancel";
-    }
-    else {
+        return input;       //when a prompt is cancelled, this is seen as null. Therefore if player cancels, the input is null, and we return the input (null)
+    } else {
         return input.toString().toLowerCase();
     }
-}
-
-let playerScore = 0;
-let computerScore = 0;
-
-function game() {
-    while(playerScore < 5 && computerScore < 5) {
-        let roundWinner = playRound();
-        if (roundWinner === "computer") {
-            ++computerScore;
-            console.log(`Pc wins! Current score: Computer: ${computerScore} Player: ${playerScore}`);
-        } 
-        if (roundWinner === "player") {
-            ++playerScore;
-            console.log(`Player wins! Current score: Computer: ${computerScore} Player: ${playerScore}`);
-        }
-        if (roundWinner === "tie") {
-            console.log(`It's a tie! Current score: Computer: ${computerScore} Player: ${playerScore}`);
-        }
-        if (roundWinner === "cancel") {
-            break ;
-        }
-    }   
-        if (playerScore === 5) {
-            console.log("Congratulations, player wins the game!");
-        }
-        if (computerScore === 5) {
-            console.log("What a shame, computer wins the game!");
-        }
 }
 
 function playRound() {
@@ -69,14 +45,40 @@ function playRound() {
         return "player";
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
         return "tie";
-
-    } else if (playerSelection === "cancel") {
-        return "cancel";
+    
+    } else if (playerSelection === null) {
+        return playerSelection;     //If player CANCELS the prompt in userPlay, this is seen as null. If the result of userPlay function is null, we cancel the round. But ONLY if the input is null. Player should still be able to enter an empty string or other values
     } else { 
-        console.log("I don't know this gesture.");
+        console.log("I don't know this gesture. Please try again or cancel.");
     }
 }
 
+let playerScore = 0;
+let computerScore = 0;
 
-//one issue left = if cancel is pressed it should cancel the loop and display cancelled message.//
-//works for now, but does not seem optimal? Have to mention it several times//
+function game() {
+    while(playerScore < 5 && computerScore < 5) {
+        let roundWinner = playRound();
+        if (roundWinner === "computer") {
+            ++computerScore;
+            console.log(`Pc wins! Current score: Computer: ${computerScore} Player: ${playerScore}`);
+        } 
+        if (roundWinner === "player") {
+            ++playerScore;
+            console.log(`Player wins! Current score: Computer: ${computerScore} Player: ${playerScore}`);
+        }
+        if (roundWinner === "tie") {
+            console.log(`It's a tie! Current score: Computer: ${computerScore} Player: ${playerScore}`);
+        }
+        if (roundWinner === null) {
+            console.log('Game cancelled');
+            break;
+        }
+    }   
+        if (playerScore === 5) {
+            console.log("Congratulations, player wins the game!");
+        }
+        if (computerScore === 5) {
+            console.log("What a shame, computer wins the game!");
+        }       
+}
